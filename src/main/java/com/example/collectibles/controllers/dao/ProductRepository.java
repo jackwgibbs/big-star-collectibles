67a3c42366d22247c49265354b45b31f7c251432
Jack Gibbs <jackgibbs66@icloud.com>
2023-12-15 16:10:33 +0000

    Add search functionality

---
+++ src/main/java/com/example/collectibles/controllers/dao/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.collectibles.controllers.dao;
+
+import com.example.collectibles.model.Product;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    @Query("select p from Product p where p.name like %:searchString%")
+    public List<Product> searchByName(@Param("searchString") String keyword);
+}
