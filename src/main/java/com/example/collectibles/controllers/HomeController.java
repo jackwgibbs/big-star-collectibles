2e8c34f628c2b96bf1e853a90bbf2fbaa5bc2d53
Jack Gibbs <jackgibbs66@icloud.com>
2023-12-15 14:27:36 +0000

    Link HTML character pages from the home page

--- src/main/java/com/example/collectibles/controllers/HomeController.java
+++ src/main/java/com/example/collectibles/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.example.collectibles.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -12,4 +13,8 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName) {
+        return "/characters/"+charName;
+    }
 }
